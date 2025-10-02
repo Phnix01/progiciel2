4d2341060d6fd512049cf246143164ec772b9bd2
Omar Farouk <fomar9235@gmail.com>
2025-10-03 00:19:35 +0100

    ajout fichier db.php

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "garage_db";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die("Erreur connexion BDD : " . $e->getMessage());
+}
+?>
